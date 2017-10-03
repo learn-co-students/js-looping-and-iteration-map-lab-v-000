8ed4669ceeaaf90a3452c204663204fa92124549
smithwebtek <brad@smithwebtek.com>
2017-10-02 22:08:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file.
+function lowerCaseDrivers(collection){
+  let newDrivers = []
+  for (const driver of collection){
+    newDrivers.push(driver.toLowerCase())
+  }  
+  return newDrivers
+}
+
+function nameToAttributes(collection){
+  let names = []
+  for (const element of collection){
+    names.push(makePair(element))   
+  }
+  return names
+}
+
+function makePair(string){
+  let first = string.split(' ')[0]
+  let last = string.split(' ')[1]
+  return { firstName:first, lastName: last }
+}
+
+function attributesToPhrase(collection){
+  let phrases = []
+  let phrase = ""
+  for (const element of collection){
+    phrase = element.name + ' is from ' + element.hometown
+    phrases.push(phrase)
+  }
+  return phrases
+}
