dc32ff3388e212000e355c1cc017277f8cfbe745
psnider30 <pbsnider@gmail.com>
2017-10-18 16:37:06 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function(driver) {
+    return driver.toLowerCase();
+  });
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(function (driver) {
+    first = driver.split(' ')[0];
+    last = driver.split(' ')[1];
+
+    return { firstName: first, lastName: last };
+  });
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function (driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
+}
