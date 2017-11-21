d14688002701608e2d0e0d411d988c8d241e217b
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-11-21 21:37:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+function lowerCaseDrivers(arr) {
+  return arr.map(function(name){
+    return name.toLowerCase();
+  })
+};
+
+function nameToAttributes(arr){
+  return arr.map(function(driver){
+    let fName = driver.split(" ")[0];
+    let lName = driver.split(" ")[1];
+    return { firstName: fName, lastName: lName };
+  })
+};
+
+function attributesToPhrase(arr) {
+  return arr.map(function(driver){
+    return `${driver.name} is from ${driver.hometown}`;
+  })
+}
