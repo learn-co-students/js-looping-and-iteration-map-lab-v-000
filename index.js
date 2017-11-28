7daf61065e00fe209a6bb927d0bf4a2fb86ea068
Brandon Hosley <brandon.hosley@csuglobal.edu>
2017-11-28 00:10:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file.
+function lowerCaseDrivers(array){
+  return array.map(function(element){return element.toLowerCase(); });
+};
+function nameToAttributes(array){
+  return array.map(function(element){
+    let name = element.split(" ");
+    return{firstName: name[0], lastName: name[1]};
+  });
+};
+function attributesToPhrase(array){
+  return array.map(function(element){
+    return `${element.name} is from ${element.hometown}`;
+  });
+};
