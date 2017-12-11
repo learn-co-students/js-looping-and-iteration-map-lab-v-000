b1112efb7142442d3ae08b8e888848a284d4408f
Terry Blue <terryblue@comcast.net>
2017-12-11 22:14:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file.
+
+// returns all drivers lowercased ‣
+// does not modify the original array
+function lowerCaseDrivers(drivers) {
+  const lcDrivers = drivers.map(name => name.toLowerCase());
+  return lcDrivers;
+}
+
+// returns list of objects with appropriate first and last names
+function nameToAttributes(driversArray) {
+  let driversObj = driversArray.map(function(name) {
+      let dObj = {};
+      nameArr = name.split(' ');
+      dObj["firstName"] = nameArr[0];
+      dObj["lastName"] = nameArr[1];
+      return dObj;
+  });
+  return driversObj;
+}
+
+// converts to list saying the name and where each individual is from
+function attributesToPhrase(driversObj) {
+
+  let driversArr = driversObj.map(function(obj) {
+     return `${obj["name"]} is from ${obj["hometown"]}`;
+
+  });
+  return driversArr;
+}
