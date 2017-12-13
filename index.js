0ff2e16c310c9c484d5ce619619dafbfb560ab20
github username <github email address>
2017-12-13 15:14:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file.
+// lowerCaseDrivers - this function takes an array of drivers, and returns an array of the drivers names in lowercase.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function (driver) {
+    return driver.toLowerCase();
+  });
+}
+
+
+// nameToAttributes - this function takes an array of drivers with their first and last name separated by a space, and
+// returns an array of JavaScript objects with firstName and lastName attributes.
+function nameToAttributes(drivers) {
+  return drivers.map(function (driver) {
+  const firstName = driver.split(" ")[0];
+  const lastName = driver.split(" ")[1];
+
+  return { firstName: firstName, lastName: lastName };
+  //this feels weird though; repeating variable names
+});
+}
+
+// attributesToPhrase - this function takes an array of drivers as JavaScript objects and returns a string saying
+// "<NAME OF DRIVER> is from <HOMETOWN>" for each JavaScript object in the array. Note that between the < and > we
+// are looking for the value stored in the name and hometown object attributes, not the strings "DRIVER" or "HOMETOWN".
+function attributesToPhrase(drivers) {
+  return drivers.map(function (driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+    // need backtick marks if doing interpolation
+  });
+}
