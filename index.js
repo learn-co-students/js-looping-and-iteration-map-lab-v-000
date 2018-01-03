93d44a6c506e257eff56d81ea0d046d3c20c01a4
Adam Tropp <adt6261@gmail.com>
2018-01-03 19:27:56 +0000

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,17 @@
 function lowerCaseDrivers(drivers) {
   return drivers.map(function (lowercase) {return lowercase.toLowerCase();});
 }
+
+function nameToAttributes(drivers) {
+  return drivers.map(function (i){
+    const first = i.split(" ")[0];
+    const last = i.split(" ")[1];
+    return { firstName: first, lastName: last};
+  })
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function (i) {
+    return `${i.name} is from ${i.hometown}`;
+  })
+}
