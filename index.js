39f4d0261d6d619df213411dc02f4354ffc015c8
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-04 18:26:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  const collection = [];
+
+  for (const driver of drivers ) {
+    collection.push(driver.toLowerCase())
+  }
+
+  return collection
+}
+
+function nameToAttributes(drivers) {
+  const collection = [];
+
+  for (const driver of drivers ) {
+    names = driver.match(/\S+/g);
+    collection.push(Object.assign({}, { firstName: names[0], lastName: names[1]}))
+  }
+
+  return collection
+}
+
+function attributesToPhrase(drivers) {
+  const collection = [];
+
+  for (const driver of drivers ) {
+    collection.push(`${driver.name} is from ${driver.hometown}`)
+  }
+
+  return collection
+}
