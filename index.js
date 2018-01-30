2a24df826cf95c1806a4f206b6b45f6d64a5eab2
github username <schun08@gmail.com>
2018-01-29 20:33:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers){
+  return drivers.map(function(driver){return driver.toLowerCase();});
+}
+
+function nameToAttributes(drivers){
+  return drivers.map(function(element){
+    const names = element.split(' ');
+    return {firstName: names[0], lastName: names[1]};
+  });
+}
+
+function attributesToPhrase(drivers){
+  // for each object in the array, add "${Name} is from ${Hometown}"
+  // iterate over array
+  // for each element create string "${element.name} is from ${element.hometown}"
+  return drivers.map(function(element){
+    return `${element.name} is from ${element.hometown}`;
+  });
+}
