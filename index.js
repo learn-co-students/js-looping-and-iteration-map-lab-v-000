d1cae49176d08497186f5fb2202674e4973fd6ef
nothingisfunny <if.the.kids.r.united@gmail.com>
2018-02-02 14:49:56 -0800

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers){
-  let newDrivers = [];
-  drivers.map(driver => {
-    newDrivers.push(driver.toLowerCase())
+  return drivers.map(function(driver){
+    return driver.toLowerCase();
   })
-  return newDrivers;
 }
 
 function nameToAttributes(names){
-  let newNames = [];
-  names.map(name =>{
+  return names.map(name =>{
     let object = {};
     object.firstName = name.split(" ")[0]
     object.lastName = name.split(" ")[1]
-    newNames.push(object);
+    return object
   })
-  return newNames;
 }
 
 function attributesToPhrase(drivers){
-  let newArray = [];
-  drivers.map(driver =>{
-    newArray.push(driver.name + " is from " + driver.hometown);
+  return drivers.map(driver =>{
+    return driver.name + " is from " + driver.hometown;
   })
-  return newArray;
 }
