c0cf07c8ee47e3fd6b2b5cecfb8bf0e93f263044
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-02-08 00:47:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drives) {
+    const low = drives.map(function(name) {
+        
+         return name.toLowerCase();
+        // debugger;
+    }) 
+    return low;
+}
+
+function nameToAttributes(drives) {
+    return drives.map(function(fullname) {
+        // const name = {};
+        const first = fullname.split(" ")[0];
+        const last = fullname.split(" ")[1];
+        
+        return {firstName: first, lastName: last};
+    })
+    
+}
+
+function attributesToPhrase(drivers) {
+    return drivers.map(function(driver) {
+        return `${driver.name} is from ${driver.hometown}`;
+    })
+    
+}
+    
