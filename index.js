c57257d244c536066a91c2c939820b39f6161bad
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-02-12 17:58:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(array) {
+  const result = array.map(function (e) {return e.toLowerCase() } );
+  return result;
+}
+
+function nameToAttributes(array) {
+  const results = array.map( function (e) {
+    newArr = e.split(" ");
+    return {firstName: newArr[0], lastName: newArr[1]};
+  })
+  return results;
+}
+
+function attributesToPhrase(array) {
+  const results = array.map(function (e) {
+    return `${e.name} is from ${e.hometown}`
+  });
+  return results;
+}
