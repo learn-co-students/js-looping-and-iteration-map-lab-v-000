f244fe135e42170625d8ac58401ca9c4b9748837
Sam Ojling <s.ojling@gmail.com>
2018-03-14 15:28:32 +0100

    Done.

--- index.js
+++ index.js
@@ -7,19 +7,13 @@ function lowerCaseDrivers(drivers) {
 
 function nameToAttributes(drivers) {
   return drivers.map(function (driver) {
-    const name = driver.split(" ");
-    const obj = {
-      firstName: name[0],
-      lastName: name[1]
-    };
-    return obj;
+    const fullName = driver.split(" ");
+    return {firstName: fullName[0], lastName: fullName[1]};
   });
 }
 
 function attributesToPhrase(drivers) {
   return drivers.map(function (driver) {
-    const name = driver.name;
-    const hometown = driver.hometown;
-    return `${name} is from ${hometown}`;
+    return `${driver.name} is from ${driver.hometown}`;
   });
 }
