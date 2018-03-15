bef254456cb9a49e39d3090d74d17e888c0ae08a
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-15 22:25:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(arr) {
+  return arr.map(function (name) { return name.toLowerCase() })
+}
+
+function nameToAttributes(arr) {
+  return arr.map(
+    function (user) {
+      splitted = user.split(' ')
+      return { firstName: splitted[0], lastName: splitted[1] }
+    }
+  )
+}
+
+function attributesToPhrase(arr) {
+  return arr.map(
+    function (user) {
+      return `${user.name} is from ${user.hometown}`
+    }
+  )
+}
