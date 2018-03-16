c6f1a70d49e12c5d9e53139cae92338362591f3e
Brittany Morris <brittmmendez@gmail.com>
2018-03-16 20:43:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 function lowerCaseDrivers(drivers){
+  //returns all drivers lowercased
+  //does not modify the original array
   return drivers.map(function (driver) { return driver.toLowerCase();});
 }
 
 function nameToAttributes (drivers) {
+  //returns list of objects with appropriate first and last names
   return drivers.map(function (driver) {
     const splitName=driver.split(" ");
     return {firstName: splitName[0], lastName: splitName[1]};
-     });
+  });
 }
 
 
 function attributesToPhrase(drivers){
-return drivers.map(function (driver) { return `${driver.name} is from ${driver.hometown}`;});
+  //converts to list saying the name and where each individual is from
+  return drivers.map(function (driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
 }
