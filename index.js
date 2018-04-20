d7dbdc280bada8aa5489e0ed1b08e7e6479a0aa8
Chrissy Hunt <chrissy.hunt@gmail.com>
2018-04-20 21:49:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(array) {
+  const newArr = array.map(function(name) { 
+    return name.toLowerCase(); 
+  });
+  
+  return newArr;
+}
+
+function nameToAttributes(array) {
+  const splitArr = array.map(function(fullName) {
+    const splitName = fullName.split(" ");
+    return Object.assign({}, { firstName: splitName[0], lastName: splitName[1]});
+  })
+  
+  return splitArr;
+}
+
+function attributesToPhrase(array) {
+  const newArr = array.map(function(obj) {
+    return `${obj['name']} is from ${obj['hometown']}`
+  });
+  
+  return newArr;
+}
