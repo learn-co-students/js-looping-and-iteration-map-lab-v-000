ed1c85beef7efdf889ac5879141025340a30fc00
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-23 21:03:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  const lowerCaseDrivers = drivers.map(function(driver) { return driver.toLowerCase() });
+  return lowerCaseDrivers;
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(function (driver) {
+    const first = driver.split(" ")[0];
+    const last = driver.split(" ")[1];
+    return {firstName: first, lastName: last};
+  }) ;
+  
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function (driver) {
+    return `${driver.name} is from ${driver.hometown}`
+  });
+}
