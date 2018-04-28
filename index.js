6d5f5d5e8e7c28576df932a398c3145b85f44ea1
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-04-28 10:08:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers){
+  return drivers.map(function (name) {return name.toLowerCase()})
+}
+
+function nameToAttributes(drivers){
+  return drivers.map(function (name) {
+    let split = name.split(" ")
+    return {firstName: split[0], lastName: split[1]}
+  })
+}
+
+function attributesToPhrase(drivers){
+  return drivers.map(function (driver) {
+    return `${driver.name} is from ${driver.hometown}`
+  })
+}
