a21223a1534fa7bc1a9a6c86d156ce2bc0ffe37b
Obafemi Fadairo <ofpupp@princeton.edu>
2018-04-29 02:14:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+
+  x = drivers.map(function (driver) { return driver.toLowerCase();});
+  return x;
+}
+
+function nameToAttributes(drivers) {
+
+  x = drivers.map(function (driver) {
+    split = driver.split(" ");
+    newObj = {};
+    newObj["firstName"] = split[0];
+    newObj["lastName"] = split[1];
+    return newObj;
+  });
+  return x;
+}
+
+function attributesToPhrase(drivers) {
+
+  return drivers.map(function (driver) {
+    return `${driver.name} is from ${driver.hometown}`
+  });
+}
