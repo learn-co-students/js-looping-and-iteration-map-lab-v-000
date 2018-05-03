a21d2bf5bca4f119b2c94111631ab4734829a1fa
jacobsilver2 <jacobsilver2@mac.com>
2018-05-03 11:09:43 -0400

    work on step 2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+    return drivers.map(function(d)
+        { return d.toLowerCase()
+        });
+}
+
+function nameToAttributes(drivers) {
+    
+    return drivers.map(function (d) {
+        const newObj = {};
+        let arr = d.split(" ");
+        //split d by space into a new array
+        //arr[0] 
+        newObj.firstName = arr[0];
+        newObj.lastName = arr[1]
+    });
+}
