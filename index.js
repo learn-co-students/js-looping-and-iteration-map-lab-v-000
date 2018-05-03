65da4d5ac354245587ba65de76ce2ddf339b9061
jacobsilver2 <jacobsilver2@mac.com>
2018-05-03 11:17:28 -0400

    Done.

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ function nameToAttributes(drivers) {
     
     return drivers.map(function (d) {
         const newObj = {};
-        const firstName = d.split(" ")[0];
-        const lastName = d.split(" ")[1];
+        const driverFirstName = d.split(" ")[0];
+        const driverLastName = d.split(" ")[1];
         //split d by space into a new array
         //arr[0] 
-        return {}
+        return {firstName: driverFirstName, lastName: driverLastName }
+    });
+}
+
+function attributesToPhrase(drivers) {
+    return drivers.map(function(d){
+        return `${d.name} is from ${d.hometown}`
     });
 }
