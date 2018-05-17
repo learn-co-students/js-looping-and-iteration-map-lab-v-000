ea105aca52e6c09c3689498c1fd49539809a4370
Eva Navarror <emanao@me.com>
2018-05-17 19:26:30 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,5 @@ function nameToAttributes(drivers){
 function attributesToPhrase(drivers){
   return drivers.map(function (driver){
     return `${driver.name} is from ${driver.hometown}`
-  }
-
-  )
+  })
 }
