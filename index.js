4699df2f828036a0327198df070d630e25955eee
Denise Pendleton <dpendl1@hotmail.com>
2018-05-27 23:22:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file.
+const drivers = ["Ray", "Ana", "Dan"];
+const oDrivers = ["Ray Charles", "Dan Man", "Ana Banana"]
+
+function lowerCaseDrivers(drivers){
+let  newDriver = drivers.map(function(driver){
+    return driver.toLowerCase()
+  })
+  return newDriver
+}
+// newest = newDrivers.map(function(driver){return Object.assign({}, { firstName: driver[0], lastName: driver[1] }) })
+theDrivers = [
+  {name: "James", hometown: "Chicago"},
+  {name: "Dee", hometown: "New York"}
+
+]
+
+function attributesToPhrase(theDrivers){
+  sentences = theDrivers.map(function(driver){
+    return `${driver.name} is from ${driver.hometown}`
+  })
+  return sentences
+}
+
+function nameToAttributes(oDrivers){
+ let newDrivers = oDrivers.map(function(driver){ return driver.split(" ")})
+return newest = newDrivers.map(function(driver){return Object.assign({}, { firstName: driver[0], lastName: driver[1] })
+  })}
