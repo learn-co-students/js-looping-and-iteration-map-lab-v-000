c6094ef291194ca2e0fcbdfdf152c8cc34cc99f0
badlychadly <chadmontoya21@yahoo.com>
2018-06-05 16:11:08 -0400

    Done.

--- index.js
+++ index.js
@@ -7,15 +7,13 @@ function lowerCaseDrivers(drivers) {
 
 function nameToAttributes(drivers) {
   return drivers.map(function (driver) {
-    let name = driver.split(" ")
-   return Object.assign({}, {firstName: name[0], lastName: name[1]})
-  })
+    let name = driver.split(" ");
+   return {firstName: name[0], lastName: name[1]};
+  });
 }
 
 function attributesToPhrase(drivers) {
-  return drivers.map(function (user_info) {
-    let name = user_info["name"]
-    let town = user_info["hometown"]
-    return `${name} is from ${town}`
-  })
+  return drivers.map(function (driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
 }
