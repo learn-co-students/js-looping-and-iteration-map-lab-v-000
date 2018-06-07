2f083b298c3540233db724b56534886d6ef473e7
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-06-06 20:40:28 -0400

    ATP

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+    return drivers.map(function(driver) {return driver.toLowerCase();});
+}
+
+function nameToAttributes(fullNames){
+    return fullNames.map(
+        function(fullName) {
+            const firstAndLastName = fullName.split(' ');
+            return {
+                firstName: firstAndLastName[0],
+                lastName: firstAndLastName[1]
+            };
+        }
+    );
+}
+
+function attributesToPhrase(drivers){
+    return drivers.map(
+        function(driver){
+            return `${driver.name} is from ${driver.hometown}`;
+        }
+    );
+}
