e0d20ea81da97ed18e84926e5b59eca888527a31
Jonathan Tiede <jon@Jonathans-MacBook-Pro.local>
2018-06-27 14:59:47 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file.
+function lowerCaseDrivers(arr) {
+	return arr.map(i => i.toLowerCase());
+}
+
+function nameToAttributes(arr) {
+	const updatedArr = [];
+	arr.forEach(function(element) {
+	    let obj = {}
+	    test = element.split(' ')
+	    obj['firstName'] = test[0]
+	    obj['lastName'] = test[1]
+	    updatedArr.push(obj)
+  	})
+return updatedArr;
+}
+
+function attributesToPhrase(drivers) {
+  const updatedArr = []
+  drivers.forEach(function(driver) {
+    updatedArr.push(`${driver.name} is from ${driver.hometown}`)
+  })
+  return updatedArr;
+}
