352b7d776d9a5aee9ecaa00da788a0ef5323189d
ArielFeingold <feingold.ariel@gmail.com>
2018-06-29 12:41:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-function lowerCaseDrivers(drivers){
-  driversNew = drivers.map(function (driver) { return driver.toLowerCase()});
-  console.log(driversNew)
-  return driversNew;
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function(driver) {
+    return driver.toLowerCase()
+  });
 };
 
 function nameToAttributes(drivers){
-  driversNew = drivers.map( function (driver) {
-    const arr = driver.split(" ")
-    return Object.assign({}, {firstName: arr[0]}, {lastName: arr[1]});
+  return drivers.map(function(driver){
+    arr = driver.split(" ")
+    return {firstName: arr[0], lastName: arr[1]};
   });
-  console.log(driversNew)
-  return driversNew;
 };
 
-function attributesToPhrase(drivers){
-  driversNew = drivers.map( function (driver) {
+function attributesToPhrase(drivers) {
+  return drivers.map(function(driver){
     return `${driver.name} is from ${driver.hometown}`
-  });
-  console.log(driversNew)
-  return driversNew;
-};
+  })
+}
