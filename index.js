7a88c2de1217037fb5fc25a2aade19323d752f0a
funemployeddad <funemployeddad@gmail.com>
2018-07-02 12:57:58 -0700

    redid lab

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 // Code your solution in this file.
-function lowerCaseDrivers (list) {
-  return list.map( function (driver){
-    return driver.toLowerCase();
+
+function lowerCaseDrivers (drivers) {
+  return drivers.map(function (driverName){
+    return driverName.toLowerCase();
   });
 }
 
 
-function nameToAttributes (list) {
-  return list.map(function (driver) {
-    const driverFirst = driver.split(' ')[0];
-    const driverLast = driver.split(' ')[1];
 
-    return { firstName: driverFirst, lastName: driverLast };
+function nameToAttributes (drivers) {
+  return drivers.map(function (driver){
+// Bobby Smith to {firstName: 'Bobby', lastName: 'Smith'}
+// need to split the string at the space, then do  a first name and last name const
+  const driverFirstName = driver.split(' ')[0];
+  const driverLastName = driver.split(' ')[1];
+
+  return { firstName: driverFirstName, lastName: driverLastName};
   });
 }
+
 
 function attributesToPhrase(drivers) {
-  return drivers.map(function (driver) {
-    return `${driver.name} is from ${driver.hometown}`;
+  return drivers.map(function (driver){
+    return `${driver.name} is from ${driver.hometown}`
   });
 }
