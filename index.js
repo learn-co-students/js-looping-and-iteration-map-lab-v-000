bee52f30e4a848fafc5ebab520eeadac5662b1b9
AspenJames <aspenjames@tqca.org>
2018-07-03 20:02:10 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(drivers) {
+  const newDrivers = drivers.map(function(driver){
+    return driver.toLowerCase();
+  });
+  return newDrivers;
+};
+
+function nameToAttributes(drivers) {
+  const newDrivers = drivers.map(function(driver){
+    return {firstName: driver.split(' ')[0], lastName: driver.split(' ')[1]}
+  });
+  return newDrivers;
+};
+
+function attributesToPhrase(drivers) {
+  const phrases = drivers.map(function(driver){
+    return `${driver.name} is from ${driver.hometown}`
+  });
+  return phrases;
+};
