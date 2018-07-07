f7518ccb9e88a12f35b400ba0680f2d5eb8488f8
Razelle McCarrick <razelle@satyaorthopedic.com>
2018-07-06 21:45:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file.
+
+
+function lowerCaseDrivers(drivers){
+  return drivers.map(x => x.toLowerCase())
+}
+
+function nameToAttributes(drivers){
+  return drivers.map(
+    x =>
+      Object.assign(
+        {
+        firstName: x.split(" ")[0],
+        lastName: x.split(" ")[1]
+        })
+  )
+}
+
+function attributesToPhrase (drivers) {
+  return drivers.map(x => `${x.name} is from ${x.hometown}`)
+}
