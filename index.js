286e1be67cdb2cf0d7122c0dfca1e588aa6736a1
MiriamPeskowitz <miriam5000@gmail.com>
2018-07-24 13:34:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file.
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function (driver) {
+    return driver.toLowerCase();
+  });
+}
+
+
+// const drivers = ['Bobby Smith', 'Sammy Watkins', 'Sally Jenkins', 'Annette Sawyer', 'Sarah Hucklebee', 'bobby ridge'];
+
+function nameToAttributes(drivers) {
+  return drivers.map(function (driver) {
+    const driverFirstName = driver.split(' ')[0];
+    const driverLastName = driver.split(' ')[1];
+    
+    return {firstName: driverFirstName, lastName: driverLastName };
+
+  });
+}
+
+ const drivers = [
+        { name: 'Bobby',   hometown: 'Pittsburgh'  },
+        { name: 'Sammy',   hometown: 'New York'    },
+        { name: 'Sally',   hometown: 'Cleveland'   },
+        { name: 'Annette', hometown: 'Los Angeles' },
+        { name: 'Bobby',   hometown: 'Tampa Bay'   }
+      ];
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function (driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+    });
+}
