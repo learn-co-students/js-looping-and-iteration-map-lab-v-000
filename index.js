b18d1ce203e32a4686441483a406d721f8beff6c
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-07-29 19:42:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(d => d.toLowerCase());
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(name) {
+    return { firstName: name.split(' ')[0], lastName: name.split(' ')[1] };
+  });
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function(driver) {
+    return driver.name + " is from " + driver.hometown;
+  });
+}
