fbf34a9faa12a4c20c4887738f5a6d9328692d27
Araba <araba.taylor20@gmail.com>
2018-08-01 21:27:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file.
+function lowerCaseDrivers(list){
+  return list.map(n=> n.toLowerCase())
+}
+
+function nameToAttributes(list){
+  return list.map(function(driver){
+    const first = driver.split(' ')[0]
+    const last = driver.split(' ')[1]
+
+    return {firstname: first, lastname: last};
+  });
+}
+
+function attributesToPhrase(list){
+  return list.map(function (driver)
+    return driver[name];
+)
+}
