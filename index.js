8be77f0b2091c77ff52c6032ed1e72a003989c17
ktamoguis <ktamoguis@gmail.com>
2018-08-08 09:04:45 +0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  const newdrivers = [];
+
+  for (const driver of drivers) {
+    newdrivers.push(driver.toLowerCase());
+  }
+  return newdrivers;
+
+}
+
+function nameToAttributes(drivers){
+  const newdrivers = [];
+
+   for (const driver of drivers){
+     const splitstring = driver.split(" ");
+     const newdriver = {};
+     newdriver.firstName = splitstring[0];
+     newdriver.lastName = splitstring[1];
+     newdrivers.push(newdriver);
+   }
+   return newdrivers;
+}
+
+function attributesToPhrase(drivers){
+  const newdrivers = [];
+
+  for (const driver of drivers){
+    const newdriver = driver['name'] + " is from " + driver['hometown'];
+    newdrivers.push(newdriver);
+  }
+  return newdrivers;
+}
