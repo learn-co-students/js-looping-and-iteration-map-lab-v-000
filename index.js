750d38e41e0471660c0b043e1a99a13d2c8ceb4e
Victoria Meng <nights1ststar@protonmail.com>
2018-08-16 00:42:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(driver => driver.toLowerCase());
+};
+function nameToAttributes(drivers) {
+  return drivers.map(function (driver) {
+    const driver_array = driver.split(" ");
+    return Object.assign({firstName: driver_array[0], lastName: driver_array[1]});
+  });
+};
+function attributesToPhrase(drivers) {
+  return drivers.map(function (driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
+};
