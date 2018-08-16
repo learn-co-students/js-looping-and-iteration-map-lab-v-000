5a1c41937969388e5e0e30f2eaa10610a0608987
Laura <laurao148@gmail.com>
2018-08-16 02:51:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  const newArrayDrivers = drivers.map(function(driver) {
+    return driver.toLowerCase();
+  })
+  return newArrayDrivers;
+}
+
+function nameToAttributes(drivers) {
+  const newList = drivers.map(function(driver) {
+    return Object.assign({}, {firstName: driver.split(" ")[0], lastName: driver.split(" ")[1]});
+  })
+  return newList;
+}
+
+function attributesToPhrase(drivers) {
+  const phrases = drivers.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  })
+  return phrases;
+}
