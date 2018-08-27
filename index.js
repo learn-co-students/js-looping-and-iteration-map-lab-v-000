2b9e6354fd30cb9227377500eacdaacd66a4caa2
Katrina Brinson <kbrinso@gmail.com>
2018-08-27 05:59:09 +0000

    working

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file.
+function lowerCaseDrivers(data){
+  return data.map(x => x.toLowerCase());
+}
+function nameToAttributes(data){
+  const newHash = {};
+  for (const i in data){
+    let name = i.split(" ");
+    
+  }
+  data.map(x => x.split(" ");
+
+  return newHash
+}
