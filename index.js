954f1d2ecafffcdc9c034bcbbe0366b016823499
arye-dov-eidelman <aryedov.e@gmail.com>
2018-09-06 12:38:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file.
+const lowerCaseDrivers = divers => [...divers].map(diver => diver.toLowerCase())
+
+const nameToAttributes = input => [...input].map(fullName =>
+  ({
+    firstName: fullName.split(' ')[0],
+    lastName: fullName.split(' ')[1]
+  })
+)
