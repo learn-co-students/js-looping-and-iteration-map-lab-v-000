18495d38a4b7a6c939b5b9b27cae5a516666e1b8
arye-dov-eidelman <aryedov.e@gmail.com>
2018-09-06 12:50:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const lowerCaseDrivers = divers => [...divers].map(diver => diver.toLowerCase())
 
-const nameToAttributes = input => [...input].map(fullName =>
-  ({
-    firstName: fullName.split(' ')[0],
-    lastName: fullName.split(' ')[1]
+const nameToAttributes = input => {
+  return [...input].map(fullName => {
+    return {
+      firstName: fullName.split(' ')[0],
+      lastName: fullName.split(' ')[1]
+    }
   })
-)
+}
+
+const attributesToPhrase = people => {
+  return [...people].map(person => `${person.name} is from ${person.hometown}`)
+}
