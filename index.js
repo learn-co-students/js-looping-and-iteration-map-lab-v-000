ca7cf90592aa1b2c0bfb57706faa9daf8e722082
hkleindl <hkleindl@gmail.com>
2018-09-27 12:05:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function(driver) { return driver.toLowerCase();});
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(driver) { 
+    return Object.assign({}, driver.split(" ")[-1], {firstName: driver.split(" ")[0], lastName: driver.split(" ")[1]});
+  });
+}
+
+function attributesToPhrase(drivers) {
+
+}
