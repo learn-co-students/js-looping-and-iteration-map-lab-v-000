b9fabb26a691b36450374420de1e01a03e216006
Jayon Kim <jpkim921@gmail.com>
2018-10-01 17:10:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file.
+
+// const drivers = [
+//         'Bobby Smith',
+//         'Sammy Watkins',
+//         'Sally Jenkins',
+//         'Annette Sawyer',
+//         'Sarah Hucklebee',
+//         'bobby ridge'
+// ];
+
+function lowerCaseDrivers(arr) {
+  return arr.map(function(driver){
+    return driver.toLowerCase();
+  });
+}
+
+
+function nameToAttributes(arr){
+  return arr.map(function(driver){
+    const driverName = driver.split(" ");
+    return Object.assign({}, {firstName: driverName[0]}, {lastName: driverName[1]});
+  });
+}
+
+// function nameToAttributes(arr){
+//   return arr.map(function(driver){
+//     const driverName = driver.split(" ");
+//     return {firstName: driverName[0], lastName: driverName[1]};
+//   });
+// }
