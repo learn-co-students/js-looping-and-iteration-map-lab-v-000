4e1224d239ab61b475d7dc3711155277b69e48b5
BrettSWeisberg <weisberg.brett@gmail.com>
2018-11-11 06:43:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file.
+const drivers = [
+  { firstName: 'Bobby', lastName: 'Smith' },
+  { firstName: 'Sammy', lastName: 'Watkins' },
+  { firstName: 'Sally', lastName: 'Jenkins' },
+  { firstName: 'Annette', lastName: 'Sawyer' },
+  { firstName: 'Sarah', lastName: 'Hucklebee' },
+  { firstName: 'bobby', lastName: 'ridge' }
+];
+
+function lowerCaseDrivers(drivers){
+  drivers.map(function (driver) {
+    return driver.firstName.toLowerCase(), driver.lastName.toLowerCase();
+  })
+}
