e52bea8d7b719cbd34cac553d860954044adc852
Anu Khambete <anukhambete@gmail.com>
2018-11-12 22:11:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array) {
+  let new_array =[];
+  new_array = array.map(x => x.toLowerCase());
+  return new_array
+}
+
+function nameToAttributes(array) {
+  var obj = array.map(function(x) {
+	   return{firstName: x.split(' ')[0],
+             lastName: x.split(' ')[1]};
+           });
+  return obj;
+}
