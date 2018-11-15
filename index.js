ed85dd4f5beb77434f154cf3d342aeab55ef32b4
BrettSWeisberg <weisberg.brett@gmail.com>
2018-11-14 20:22:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 // Code your solution in this file.
-const drivers = [
-  { firstName: 'Bobby', lastName: 'Smith' },
-  { firstName: 'Sammy', lastName: 'Watkins' },
-  { firstName: 'Sally', lastName: 'Jenkins' },
-  { firstName: 'Annette', lastName: 'Sawyer' },
-  { firstName: 'Sarah', lastName: 'Hucklebee' },
-  { firstName: 'bobby', lastName: 'ridge' }
-];
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
 
 function lowerCaseDrivers(drivers){
-  drivers.map(function (driver) {
-    return firstName.toLowerCase(), lastName.toLowerCase();
+  return drivers.map(function (driver) {
+    return driver.toLowerCase();
+  })
+}
+
+const driver = ['Bobby Smith', 'Sammy Watkins','Sally Jenkins','Annette Sawyer']
+
+function nameToAttributes(drivers){
+  return drivers.map(function(driver) {
+    var array = driver.split(' ');
+    return driver = { firstName: array[0], lastName: array[1] }
+})
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function(driver) {
+    return driver.name +" is from " + driver.hometown;
   })
 }
