6cc160a27c2f8b354f84ef0cd5159f452ee17d3a
Will Schultz <will.j.schultz@gmail.com>
2018-11-30 04:57:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file.
+function lowerCaseDrivers(collection) {
+  return collection.map(function(driver) {
+    return driver.toLowerCase();
+  });
+}
+
+function nameToAttributes(collection) {
+  return collection.map(function(driver) {
+    const nameFirst = driver.split(' ')[0];
+    const nameLast = driver.split(' ')[1];
+
+    return { firstName: nameFirst, lastName: nameLast };
+  });
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
+}
