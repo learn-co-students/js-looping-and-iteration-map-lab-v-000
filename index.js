0ba0758b07fed0e487d5f71f6f5faf992850e48e
Alexandra Fren <alexandra.fren@gmail.com>
2018-12-10 19:52:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array) {
+  return array.map(function(lower) { return lower.toLowerCase();});
+}
+
+function nameToAttributes(array) {
+  return array.map(function(combine) {
+    let firstName = combine.split(" ")[0];
+    let lastName = combine.split(" ")[1];
+    return {firstName: firstName, lastName: lastName};
+  });
+}
+
+function attributesToPhrase(array) {
+  return array.map(function(print){
+    return( print.name + " is from " + print.hometown);
+  })
+}
