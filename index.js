a6bb4ffb385a094c993936efe24d6dfa7e223684
Christopher P. Dundon <chris.dundon@gmail.com>
2018-12-11 21:02:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file.
+const drivers = [];
+
+function lowerCaseDrivers() {
+	const newArr = drivers.slice(0);
+	
+//	function lowerCaseItems(item) {
+//		return item.toLowerCase();
+//	}
+
+//	newArr.map(lowerCaseItems);
+
+	newArr.map(function (val) { return val.toLowerCase()});
+	return newArr;
+}
