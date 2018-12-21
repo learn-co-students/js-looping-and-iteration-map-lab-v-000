d31275debb1965dab74241c45eec8cf17b6370d6
Kachel Wittig <kjwittig@gmail.com>
2018-12-21 01:01:39 -0600

    Donezo

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(drivers) {
+  return drivers.map(driver => driver.toLowerCase())
+}
+
+function nameToAttributes(drivers) {
+  const names = drivers.map(function (driver) {
+    return Object.assign({}, { firstName: driver.split(" ")[0] }, { lastName: driver.split(" ")[1] })
+   })
+   return names
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(driver => `${driver.name} is from ${driver.hometown}`)
+}
