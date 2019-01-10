f1b10514b587574866233309229c03f4470729b0
nichia <nichia@gmail.com>
2019-01-10 10:29:32 -0500

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ function lowerCaseDrivers(drivers) {
 }
 
 function nameToAttributes(drivers) {
+  // return drivers.map(function(driver) {
+  // const driverFirst = driver.split(' ')[0];
+  // const driverLast = driver.split(' ')[1];
+  //
+  // return { firstName: driverFirst, lastName: driverLast };
+
   const newObject = drivers.map(function(name) {
     const words = name.split(" ");
     return Object.assign({}, { firstName: words[0], lastName: words[1]});
