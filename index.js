b1650435eb6aa531845384889807477979d732b5
atxrenegade <harleighabel@gmail.com>
2019-01-29 21:34:15 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file.
+function lowerCaseDrivers(drivers){
+	return drivers.map(driver => driver.toLowerCase());
+}
+
+function nameToAttributes(drivers){
+	return drivers.map((driver) => {
+		let firstName = driver.split(" ")[0];
+		let lastName = driver.split(" ")[1];
+		return { firstName: firstName, lastName: lastName}
+	})
+};
+
+function attributesToPhrase(drivers) {
+	return drivers.map((driver) => {
+		return (`${driver.name} is from ${driver.hometown}`);
+	})
+};
+
+
+
+
+/* flatiron legacy code above vs my code below
+
+const nameToAttributes = drivers.map((driver) => {
+  let driverNameSplit = (driver.split(" "))
+  return {firstName: driverNameSplit[0], lastName: driverNameSplit[1]}
+});
+*/
