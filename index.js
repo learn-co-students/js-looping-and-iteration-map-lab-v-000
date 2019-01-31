fa12150a650e634874a26e09131161dbaa0075cf
daniellecroxton <danikrist@gmail.com>
2019-01-30 20:29:04 -0700

    Done.

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ function nameToAttributes (drivers) {
 }
 
 function attributesToPhrase (drivers) {
-  return drivers;
-    // return drivers.map(function(driver) {
-    //   const driverFirstName = driver.split(" ")[0]
-    //   const driverLastName = driver.split(" ")[1]
-    // return { firstName: driverFirstName, lastName: driverLastName };
+    return drivers.map(function(driver) {
+      return driver.name + ' is from ' + driver.hometown;
   });
 }
