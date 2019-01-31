d235f16e2ed2cfd34e368fa2ed671c1cf59d3c6d
daniellecroxton <danikrist@gmail.com>
2019-01-30 20:11:33 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file.
+function lowerCaseDrivers (drivers) {
+    return drivers.map(function(driver) {
+    return driver.toLowerCase();
+  });
+}
+
+function nameToAttributes (drivers) {
+    var splitName = drivers.split(" ");
+    var firstName = splitName.slice (0, -1).join(" ");
+    var lastName = splitName.pop();
+    return drivers.map(function(driver) {
+    return Object.assign({}, driver, { firstName: firstName, lastName: lastName });
+  });
+}
+
+//
+// const newEngineers = map(oldAccounts, function(account) {
+//     return Object.assign({}, account, { accessLevel: 'admin' });
+// });
