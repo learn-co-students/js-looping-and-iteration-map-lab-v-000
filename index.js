e844715652114dd75af6e0c4ca97e4fc75c79856
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-02-06 13:23:10 -0600

    Done.

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ function lowerCaseDrivers(array) {
 
 function nameToAttributes(array) {
     return array.map(function(driver){
-        const first = driver.split(' ')[0];
-        const last = driver.split(' ')[1];
+        // const first = driver.split(' ')[0];
+        // const last = driver.split(' ')[1];
 
-        return { firstName: first, lastName: last}
+        // return { firstName: first, lastName: last}
+        return Object.assign({}, { firstName: driver.split(' ')[0], lastName: driver.split(' ')[1] })
     });
-}
+};
 
 function attributesToPhrase(array) {
     return array.map(function(driver){ 
         return `${driver.name} is from ${driver.hometown}`
     });    
-}
+};
