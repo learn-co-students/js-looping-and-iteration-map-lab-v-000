039d02ad6d34f2daaeaff2462948239519d0c0dd
github username <github email address>
2019-02-07 10:37:34 -0500

    Done.

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ function lowerCaseDrivers(drivers, element) {
   return drivers.map(d => d.toLowerCase());
 }
 
-// function nameToAttributes(drivers) {
-//   const separateDriverNames = drivers.map(d => d.split(" "));
-//   const finalDrivers = separateDriverNames.map(function(driver) {
-//     return Object.assign({}, driver, { "firstName": `${d[0]}` "lastName": `${d[1]}` });
-//   }
+function nameToAttributes(drivers) {
+  const finalDrivers = drivers.map(function(driver) {
+    return Object.assign({}, { "firstName": `${driver.split(" ")[0]}`, "lastName": `${driver.split(" ")[1]}` });
+  })
+  return finalDrivers;
+}
+
+function attributesToPhrase(drivers, element) {
+  return drivers.map(d => `${d.name}` + ' is from ' + `${d.hometown}`);
+}
+// function attributesToPhrase(drivers) {
+//   const driversWithHometowns = drivers.map(function(driver) {
+//     return Object.assign({}, driver, `${driver.name}`+ 'is from' + `${driver.hometown}` );
+//   })
+//   return driversWithHometowns;
 // }
