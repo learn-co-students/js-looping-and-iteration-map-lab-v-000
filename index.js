79c9acac2e9b8dfb75e7a460b1643b9b909ddfd2
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2019-02-07 23:35:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file.
+function lowerCaseDrivers(array) {
+  return array.map(function(element) { return element.toLowerCase() });
+}
+
+function nameToAttributes(array) {
+  return array.map(function(element) {
+    const firstElement = element.split(" ")[0];
+    const secondElement = element.split(" ")[1];
+    return { firstName: firstElement, lastName: secondElement };
+  });
+}
