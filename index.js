002af0f666655a31b5c978873673cb80f4e7eb8c
Pamela Frieze <pfrieze@gmail.com>
2019-02-07 21:54:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
-function lowerCaseDrivers(drivers) {
-  return drivers.map(function(driver) {
+function lowerCaseDrivers(employees) {
+  return employees.map(function(driver) {
     return driver.toLowerCase();
   });
 }
