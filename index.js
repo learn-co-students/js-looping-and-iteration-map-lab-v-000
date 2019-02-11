af94f727ba401175ba9040b96953d6c14de0ea69
Christine Cua <cloudycua@gmail.com>
2019-02-11 18:00:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
 function lowerCaseDrivers(list) {
   return list.map(function(driver) {
     return driver.toLowerCase();
   });
 }
+
+// const drivers = [ 'Bobby Smith', 'Sammy Watkins' ]
+
+// returns [
+//   { firstName: 'Bobby', lastName: 'Smith' },
+//   { firstName: 'Sammy', lastName: 'Watkins' },
+// ]
 
 function nameToAttributes(list) {
   return list.map(function(driver) {
@@ -12,6 +21,13 @@ function nameToAttributes(list) {
       };
   });
 }
+
+// const drivers = [
+//   { name: 'Bobby', hometown: 'Pittsburgh' },
+//   { name: 'Sammy', hometown: 'New York' },
+// ]
+
+// returns [ 'Bobby is from Pittsburgh', 'Sammy is from New York' ]
 
 function attributesToPhrase(list){
   return list.map(function(driver) {
