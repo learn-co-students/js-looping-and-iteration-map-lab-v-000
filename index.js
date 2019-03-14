972b32467b41acb2cc241d62518eec8c65fc6242
Steve Albonico <salbonico@gmail.com>
2019-03-14 13:50:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array) {
+let newarray = []
+array.map(function(driver){
+newarray.push(driver.toLowerCase())
+})
+return newarray
+}
+
+
+function nameToAttributes(array){
+  let newarray = []
+  array.map(function(driver){
+    let temparray = driver.split(" ")
+    newarray.push({firstName:temparray[0], lastName:temparray[1]})
+  })
+  return newarray
+}
+
+function attributesToPhrase(array){
+  let newarray = []
+  array.map(function(driver){
+    newarray.push(`${driver.name} is from ${driver.hometown}`)
+  })
+  return newarray
+}
