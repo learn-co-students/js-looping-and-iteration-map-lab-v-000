1ebdeafbd6a73b58b67a57cd1760e2f1a500f480
sjwakean <swakeman@ca.rr.com>
2019-05-14 14:16:20 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+    return drivers.map(function(lowerCaseDrivers) {
+        return lowerCaseDrivers.toLowerCase();
+    });
+}
+
+function nameToAttributes(drivers) {
+    return drivers.map(function(driverNames) {
+        const first = driverNames.split(' ')[0];
+        const last = driverNames.split(' ')[1];
+            return {firstName: first, lastName: last};
+    }); 
+}
+
+function attributesToPhrase(drivers) {
+    return drivers.map(function(driver) {
+        return driver.name + " is from " + driver.hometown;
+        })
+}
+
+
+
+ 
