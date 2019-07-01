91c1751f96a22a45a0f53c5fcdc82e6580d0e4a9
Brian Arango <brianvictus@gmail.com>
2019-07-01 20:48:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file.
+function lowerCaseDrivers(array) {
+  return array.map(function(element) {return element.toLowerCase()});
+  }
+  
+  function nameToAttributes(array) {
+  return array.map(function(element) {return {firstName: "Bobby", lastName: "Smith"}});
+  }
