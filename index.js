0e04a235ee936474535762235ec9d45432313c1b
Brian Arango <brianvictus@gmail.com>
2019-07-03 02:58:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
+
 function lowerCaseDrivers(array) {
   return array.map(function(element) {return element.toLowerCase()});
   }
   
-  function nameToAttributes(array) {
-  return array.map(function(element) {return {firstName: "Bobby", lastName: "Smith"}});
+function nameToAttributes(array) {
+  return array.map(function(element) {
+    const firstName = element.split(" ")[0];
+    const lastName = element.split(" ")[1];
+    return {firstName: firstName, lastName: lastName}});
   }
+  
+function attributesToPhrase(array) {
+  return array.map(function(element) {
+    const name = element.name;
+      const hometown = element.hometown;
+        return element.name + " is from " + element.hometown;
+    }
+  );
+}
+  
+
+function map(array, callback) {
+  const newArr = [];
+  
+  for (const element of array) {
+    newArr.push(callback(element));
+  }
+}
+
+
