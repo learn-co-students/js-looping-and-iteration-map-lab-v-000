576e7e4601546b74a0d6329899d113c719ec2b0a
github username <github email address>
2019-07-22 20:58:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 function lowerCaseDrivers(drivers) {
-  const newDrivers = drivers.map( function (driver) {
+  const newDrivers = drivers.map(function(driver) {
     return driver.toLowerCase();
   })
-  return newDrivers
+  return newDrivers;
 }
 
 function nameToAttributes(names) {
   const newNames = names.map(function(name) {
-    return Object.assign({}, {firstName: name.split(" ")[0], lastName: name.split(" ")[1]})
+    return Object.assign({}, {firstName: name.split(" ")[0], lastName: name.split(" ")[1]});
   })
-  return newNames
+  return newNames;
 }
 
 function attributesToPhrase(drivers) {
   const phrase = drivers.map(function(driver) {
-    return `${driver.name} is from ${driver.hometown}`
+    return `${driver.name} is from ${driver.hometown}`;
   })
-  return phrase 
+  return phrase;
 }
