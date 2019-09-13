a30a8f8595508b0f79b7ea6acc82d14e25007bde
AlwinaO <aoyewole2586@gmail.com>
2019-09-13 15:19:42 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file.
+ function lowerCaseDrivers(drivers) {
+    let shortCaseNameDriver = drivers.map(function(driver) {
+        return driver.toLowerCase();
+    });
+    return shortCaseNameDriver;
+ }
+
+ function nameToAttributes(drivers) {
+     let driversObject = Object.assign({}, ...drivers.map(driver => ({ [firstName]: driverFirst, [lastName]: driverLast}))
+        
+        )
+        console.log(driversObject);
+     const firstAndLastName = `${driver.firstName} + " " + ${driver.lastName}`;
+    return driversObject;
+
+    return firstAndLastName;
+
+    // const driversToObject = (drivers, firstName, lastName) =>
+    //   Object.assign({}, ...arr.map(driver => (
+    //       {[driver[firstName]]: driverFirst, 
+    //        [driver[lastName]]: driverLast
+    //   })))
+    //  let firstAndLastName = `${driver.firstName} + " " + ${driver.lastName}`;
+    //   return driversToObject;
+    //   return firstAndLastName;
+ }
+
