9d60182e9b4366773274feac4377c6725201cb21
AlwinaO <aoyewole2586@gmail.com>
2019-09-14 21:52:15 +0100

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 // Code your solution in this file.
- function lowerCaseDrivers(drivers) {
+function lowerCaseDrivers(drivers) {
     let shortCaseNameDriver = drivers.map(function(driver) {
         return driver.toLowerCase();
     });
     return shortCaseNameDriver;
  }
 
- function nameToAttributes(drivers) {
-     let driversObject = Object.assign({}, ...drivers.map(driver => ({ [firstName]: driverFirst, [lastName]: driverLast}))
+function nameToAttributes(drivers) {
+    return drivers.map(function(driver){
         
-        )
-        console.log(driversObject);
-     const firstAndLastName = `${driver.firstName} + " " + ${driver.lastName}`;
-    return driversObject;
-
-    return firstAndLastName;
-
-    // const driversToObject = (drivers, firstName, lastName) =>
-    //   Object.assign({}, ...arr.map(driver => (
-    //       {[driver[firstName]]: driverFirst, 
-    //        [driver[lastName]]: driverLast
-    //   })))
-    //  let firstAndLastName = `${driver.firstName} + " " + ${driver.lastName}`;
-    //   return driversToObject;
-    //   return firstAndLastName;
+        const name = driver.split(' ')
+        return { firstName: name[0], lastName: name[1] };
+    });
  }
 
+function attributesToPhrase(drivers) {
+    return drivers.map(function(driver){
+
+        // const driverInfo = driver.split(' ')
+        // let name = driver[0];
+        // let hometown = driver[1];
+
+        return `${driver.name} is from ${driver.hometown}`;
+    });
+}
