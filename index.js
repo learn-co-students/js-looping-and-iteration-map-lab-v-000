1b8c6574eb3349f9a8cf0df34cfd9901c8771107
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-09-19 20:20:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file.
+
+const lowerCaseDrivers = (arr) => {
+  return arr.map(val => val.toLowerCase());
+};
+
+const nameToAttributes = (arr) => {
+  return arr.map(driver => {
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+    
+    return {
+      firstName: driverFirst,
+      lastName: driverLast
+    }
+  })
+};
+
+const attributesToPhrase = (obj) => {
+  return obj.map(objVal => {
+    return `${objVal.name} is from ${objVal.hometown}`;
+  });
+};
