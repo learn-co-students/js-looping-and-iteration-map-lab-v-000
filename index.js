21dcb895bbccd40119426d6e1267484df3b0e5e1
RonSala <ron.sala@gmail.com>
2020-03-03 16:23:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function lowerCaseDrivers(drivers) {
-  
+  return drivers.map(e => e.toLowerCase());
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(e => {
+    return { firstName: e.split(' ')[0], lastName: e.split(' ')[1] }
+  })
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(e => {
+    return e.name + ' is from ' + e.hometown;
+  })
 }
